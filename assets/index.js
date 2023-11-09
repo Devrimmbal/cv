b6749e511902aed88ff8d70282676e04873e2c86
Devrim Bal <devrim.bal@marsathletic.com>
2023-11-09 09:00:49 +0300

    fix-old-files

--- assets/index.js
+++ assets/index.js
@@ -24,35 +24,7 @@ switchThemeButtons = () => {
 };
 
 window.onload = () => {
-  // getData();
   switchThemeButtons();
-};
-
-getData = () => {
-  fetch("./assets/data.json")
-    .then((response) => response.json())
-    .then((json) => {
-      var ul = document.getElementById("projects");
-      for (let i = 0; i < json.length; i++) {
-        let li = document.createElement("li");
-        li.innerHTML = ` ${json[i].title} `;
-        if (json[i].tags != undefined && json[i].tags.length > 0) {
-          li.appendChild(createTags(json[i].tags));
-          li.className = "project";
-        }
-        ul.appendChild(li);
-      }
-    });
-};
-
-createTags = (data) => {
-  let ul = document.createElement("ul");
-  for (let t = 0; t < data.length; t++) {
-    let tagLi = document.createElement("li");
-    tagLi.innerHTML = ` ${data[t]} `;
-    ul.appendChild(tagLi);
-  }
-  return ul;
 };
 
 openPdf = () => {
